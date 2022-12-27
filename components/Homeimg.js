bb5e49b82e00123650a05b242b98b070f6820f79
muhammed rinshad s r <93196049+daredevilstudio@users.noreply.github.com>
2022-12-27 20:41:56 +0530

    first commit

--- components/Homeimg.js
+++ components/Homeimg.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Homeimg() {
     return (
         <div className="hidden w-100 md:w-1/2 m-auto justify-center items-center md:block ">
-            <img src="https://imgs.search.brave.com/m87xruLfKQAc7N2sT0FsllZOSY8OTHeBqa2B238Km2M/rs:fit:1200:814:1/g:ce/aHR0cHM6Ly9jZG4u/a2Vsc2V5YmFzc3Jh/bmNoLmNvbS93cC1j/b250ZW50L3VwbG9h/ZHMvY29uY3JldGUt/bW9kdWxhci1ob21l/cy1ob21lLWRlc2ln/bi1pZGVhc18zMzcw/MTEuanBn" alt="Picture of the author" width={500} height={500} />
+            <img src="https://github.com/DareDevilStudios/sharon-2.0/blob/e727d4abd443e161625f4cea8c7c54bff62c1fbf/public/home.png?raw=true" alt="Picture of the author" width={500} height={500} />
         </div>
     )
 }
