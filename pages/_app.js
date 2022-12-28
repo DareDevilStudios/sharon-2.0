fcb128fccb3edcf902fd52cd88a8e752c2bd5276
muhammed rinshad s r <93196049+daredevilstudio@users.noreply.github.com>
2022-12-28 10:08:07 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import store from '../store'
 import { Provider } from 'react-redux'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
+      <Analytics />
     </Provider>
   )
 }
