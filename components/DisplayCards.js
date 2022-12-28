7d380230d3c00cb1ca855893b4097a4c92304455
muhammed rinshad s r <93196049+daredevilstudio@users.noreply.github.com>
2022-12-28 12:34:50 +0530

    first commit

--- components/DisplayCards.js
+++ components/DisplayCards.js
@@ -6,10 +6,8 @@ function DisplayCards(props) {
         <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-5 justify-end w-full items-center">
             {props.imageUrls.map((imageUrl) => (
 
-                <div class="max-w-sm bg-white border border-sharon-or rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                    <div>
-                        <img class="rounded-t-lg scale-100 hover:scale-105 ease-in duration-500" src={imageUrl.productUrl} alt="" />
-                    </div>
+                <div class="max-w-sm max-h-sm bg-white border border-sharon-or rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                        <img class="rounded-t-lg scale-100 hover:scale-105 ease-in duration-500 h-full " src={imageUrl.productUrl} alt="" />
                 </div>
             )
             )}
