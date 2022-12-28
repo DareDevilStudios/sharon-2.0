b6597e54997b4ef6297d9918a5ac85a7cfb376ed
muhammed rinshad s r <93196049+daredevilstudio@users.noreply.github.com>
2022-12-28 15:37:30 +0530

    first commit

--- pages/login.js
+++ pages/login.js
@@ -35,24 +35,24 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className="bg-black h-screen">
-                <section class="bg-gray-50 dark:bg-gray-900">
+                <section class=" bg-gray-900">
                     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                        <a href="#" class="flex items-center mb-6 text-2xl font-semibold  text-white">
                                 SHARON INDUSTRIES
                         </a>
-                        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div class="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
                             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                <h1 class="text-xl font-bold leading-tight tracking-tight  md:text-2xl text-white">
                                     Sign in to your Admin
                                 </h1>
                                 <form class="space-y-4 md:space-y-6">
                                     <div>
-                                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">username</label>
-                                        <input type="text" value={user} onChange={(e) => setUser(e.target.value)} name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required=""/>
+                                        <label for="email" class="block mb-2 text-sm font-medium  text-white">username</label>
+                                        <input type="text" value={user} onChange={(e) => setUser(e.target.value)} name="name" id="name" class=" border   sm:text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="username" required=""/>
                                     </div>
                                     <div>
-                                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                                        <label for="password" class="block mb-2 text-sm font-medium  text-white">Password</label>
+                                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" placeholder="••••••••" class=" border   sm:text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" required=""/>
                                     </div>
                                     <button type="button" onClick={checker} class="w-full text-white bg-sharon-or focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Sign in</button>
                                 </form>
